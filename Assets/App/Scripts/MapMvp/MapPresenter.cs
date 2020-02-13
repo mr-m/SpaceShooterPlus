9340667ddf7a17c5861a0fa61f764b7601a8f353
Dmitriy Trifonov <trifonov.dmitriy.m@gmail.com>
2020-02-13 22:40:44 +0700

    Rename view and model fields in MapPresenter

--- Assets/App/Scripts/MapMvp/MapPresenter.cs
+++ Assets/App/Scripts/MapMvp/MapPresenter.cs
@@ -7,27 +7,28 @@ namespace SpaceShooterPlus.MapMvp
 {
     public class MapPresenter
     {
-        private readonly MapView mapView;
-        private readonly MapModel mapModel;
+        private readonly MapView view;
+        private readonly MapModel model;
+
         public IObservable<MarkerModel> OnButtonClickAsObservable;
 
-        public MapPresenter(MapView view, MapModel model)
+        public MapPresenter(MapView mapView, MapModel mapModel)
         {
             Debug.Log($"{nameof(MapPresenter)}.{nameof(MapPresenter)}()");
 
-            this.mapView = view;
-            this.mapModel = model;
+            this.view = mapView;
+            this.model = mapModel;
 
             var zip = Observable.Zip(
-                view.MarkerViews.ToObservable(),
-                model.MarkerModels.ToObservable(),
+                this.view.MarkerViews.ToObservable(),
+                this.model.MarkerModels.ToObservable(),
                 (x, y) =>
                 {
                     Debug.Log($"Zip: {x} + {y}");
                     return new MarkerPresenter(x, y);
                 });
 
-        this.OnButtonClickAsObservable = zip
+            this.OnButtonClickAsObservable = zip
                 .Select(x => x.OnButtonClickAsObservable)
                 .Concat();
 
